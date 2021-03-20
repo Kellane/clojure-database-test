0a009cd657f318dbcdad1d9addb9947237bcd354
Kellane Lopes <kellanelopes2000@gmail.com>
2021-03-20 10:41:19 -0300

    chore: Init project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject clojure-database-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/java.jdbc "0.7.12"]]
+  :main ^:skip-aot clojure-database-test.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
