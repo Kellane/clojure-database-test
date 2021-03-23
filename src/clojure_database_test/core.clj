a6f956a07c59d6e4f7772d66e7e64fe4cf89d82b
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2021-03-23 11:58:48 -0300

    chore: adjusts file structure

--- src/clojure_database_test/core.clj
+++ src/clojure_database_test/core.clj
@@ -1,22 +1,26 @@
 (ns clojure-database-test.core
+  (:require 
+    [clojure-database-test.transactions]
+    [clojure.java.io :as io]
+    [clojure.java.jdbc :as jdbc]
+    [clojure.data.csv :as csv]
+  )
   (:gen-class))
 
-(load-file "src/transactions/src/make-explicit/explicit.clj")
-
-(defn transaction_explicit []
-  (println "Running explicit transaction...")
-  (let [start# (System/currentTimeMillis)]
-    
-    (transaction/insert_explicit "data/data.csv")
-    
-    (let [amountTime (double (/ (- (System/currentTimeMillis) start#) 1000))]
-      (println "Explicit transaction took" amountTime "seconds")
-    )
-  )
-)
+(def dbspec-explicit {
+  :dbtype "postgresql"
+  :dbname "bd2"
+  :user "postgres"
+  :port 5432
+  :password ""
+  :auto-commit false
+})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (transaction_explicit)
+  (with-open [data (io/reader input-file)]
+    ;(.setAutocommit db false) n funcionou
+    (transactions/insert-explicit (csv/read-csv data) dbspec-explicit)
+  )
 )
---
+++ src/clojure_database_test/transactions.clj
@@ -0,0 +1,22 @@
+(ns transactions
+  (:require
+    [clojure.java.jdbc :as jdbc]
+  )
+  (:gen-class))
+
+(defn insert [row, t-conn]
+  (jdbc/insert! t-conn :product {:eid (Integer. (get row 0)) :description (get row 1)})
+)
+
+(defn insert-explicit [data, dbspec]
+  (jdbc/with-db-transaction [t-conn dbspec]
+    (try
+      (doseq [row data]
+        (insert row t-conn)
+      )
+      (catch Exception e
+        (throw e)
+      )
+    )
+  )
+)
