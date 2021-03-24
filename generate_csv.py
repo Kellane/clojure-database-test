de14df19e78829779c85f9ce5a79013b1031b69e
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-03-24 03:06:02 -0300

    feat(data): adds data files with different ammount of files

--- generate_csv.py
+++ generate_csv.py
@@ -9,20 +9,26 @@ import csv
 import sys
 
 # n = sys.argv[1]
-eid = 0
+eid = 68000
+ammount = 50000
+final = eid + ammount
 
 with open("/tmp/data.csv", "r") as src_file:
-    with open("data.csv", "w") as data_file:
+    with open("data-{}.csv".format(ammount), "w") as data_file:
         csv_writer = csv.writer(data_file)
         csv_reader = csv.reader(src_file)
 
+        for i in range(5):
+            src_file.seek(0)
+            skip_header = True
+            
+            for row in csv_reader:
+                if skip_header:
+                    skip_header = False
+                    continue
 
-        skip_header = True
-        for row in csv_reader:
-            if skip_header:
-                skip_header = False
-                continue
+                if eid == final:
+                    break
 
-            print(row[0])
-            csv_writer.writerow([eid, str(row[0])])
-            eid += 1
+                csv_writer.writerow([eid, str(row[0])])
+                eid += 1
