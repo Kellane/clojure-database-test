f0bfa306c03373055a115bd15ffdc6fd8cba3a43
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-03-24 03:05:05 -0300

    feat: adds database setup script

---
+++ setup.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE db2;
+
+\c db2
+
+CREATE ROLE clojure_user WITH PASSWORD '123456';
+ALTER ROLE clojure_user WITH LOGIN;
+
+DROP TABLE IF EXISTS product;
+CREATE TABLE product (
+    eid INTEGER PRIMARY KEY NOT NULL,
+    description TEXT
+);
+
+GRANT ALL PRIVILEGES ON product TO clojure_user;
